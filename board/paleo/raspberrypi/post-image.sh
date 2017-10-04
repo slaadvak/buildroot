174c7b82a4a6b49b8788a6389674e7d37113f146
Vincent Mercier <vincent.mercier@gmail.com>
2017-10-03 21:58:51 -0400

    Imported changed from paleo into paleo2017.08 branch

---
+++ board/paleo/raspberrypi/post-image.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+case "${2}" in
+	--add-pi3-miniuart-bt-overlay)
+	if ! grep -qE '^dtoverlay=' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
+		echo "Adding 'dtoverlay=pi3-miniuart-bt' to config.txt (fixes ttyAMA0 serial console)."
+		cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# fixes rpi3 ttyAMA0 serial console
+dtoverlay=pi3-miniuart-bt
+__EOF__
+	fi
+	;;
+esac
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage                           \
+	--rootpath "${TARGET_DIR}"     \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+rm -rf ${BINARIES_DIR}/sdcard.img.zip
+zip ${BINARIES_DIR}/sdcard.img.zip ${BINARIES_DIR}/sdcard.img
+
+
+exit $?
